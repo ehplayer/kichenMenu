2cce78f97e051ca38eec06b4474860bbf80b124d
kth <k.taehyung@navercorp.com>
2020-02-20 15:20:49 +0900

    "$TODAY"

--- updateMenu.sh
+++ updateMenu.sh
@@ -3,5 +3,5 @@ TODAY=date +%Y%m%d
 wget -x http://10.113.112.16:3002/todayFood -O todayFood.json
 python3 updateMenu.py
 git add *
-git commit -m "${TODAY} commit"
+git commit -m '"$TODAY"'
 git push origin master
